e68d40fea5920ea8e73289be3e6ec89069588819
alexburnet <alexburnet03@gmail.com>
2024-07-08 11:58:20 +0100

    "Initial Commit"

---
+++ src/main/java/com/example/machinelogapi/MachineLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.machinelogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineLogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachineLogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/machinelogapi/MachineLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.machinelogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineLogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
