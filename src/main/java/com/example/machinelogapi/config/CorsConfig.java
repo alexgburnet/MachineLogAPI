cc8122cf3195b985d90b1575682731394e93b454
alexburnet <alexburnet03@gmail.com>
2024-07-11 14:46:30 +0100

    Allow requests from all devices

--- src/main/java/com/example/machinelogapi/config/CorsConfig.java
+++ src/main/java/com/example/machinelogapi/config/CorsConfig.java
@@ -11,10 +11,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")  // Replace with your React app's URL
+                .allowedOrigins("*") // or specify your React app's IP address
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
 }
+
